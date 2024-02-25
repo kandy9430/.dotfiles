4a96bf040dec65c214ad898a583bcd8f3a7b6b1f
Andrew Keane <kandy9495@gmail.com>
2024-02-25 16:34:45 -0700

    update rclone cmds for bash

--- bash/.bashrc.d/rclone.sh
+++ bash/.bashrc.d/rclone.sh
@@ -1,7 +1,10 @@
 # mount OneDrive and OneDriveCrypt
 rclone_mount () {
-	rclone mount OneDrive: ~/OneDrive --daemon --vfs-cache-mode writes
-	rclone mount OneDriveCrypt: ~/OneDriveCrypt --daemon --vfs-cache-mode writes
+	echo -n "Rclone Password:"
+	read -s password
+	echo
+	rclone --password-command "echo $password"  mount OneDrive: ~/OneDrive --daemon --vfs-cache-mode writes
+	rclone --password-command "echo $password" mount OneDriveCrypt: ~/OneDriveCrypt --daemon --vfs-cache-mode writes
 }
 
 rclone_umount () {
