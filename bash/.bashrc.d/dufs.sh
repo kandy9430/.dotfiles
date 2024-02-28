a19aecf2097da2ee341c31674d9f549862b6b07b
Andrew Keane <kandy9495@gmail.com>
2024-02-28 10:34:00 -0700

    Changes to DUFS bash scripts

--- bash/.bashrc.d/dufs.sh
+++ bash/.bashrc.d/dufs.sh
@@ -2,7 +2,12 @@ dufs-upload() {
 	doc_dir="Documents/"
 	start_dir=$(pwd)
 
-	echo "Choose from following directories to upload, or press enter to upload all"
+	echo -n "DUFS username: "
+	read username
+	echo -n "DUFS password: "
+	read -s password
+	echo 
+	echo -n "Choose from following directories to upload, or press enter to upload all"
 	find ~/$doc_dir -maxdepth 1 -type d -printf "%P\n"
 	echo -n ": "
 	read directory
@@ -11,6 +16,6 @@ dufs-upload() {
 	#echo "Documents/$directory/"
 	
 	cd ~/$doc_dir$directory	
- 	find ~/$doc_dir$directory/ -type f -printf "%P\n" | xargs -I % curl -v -u andrew:DUFS@ndrew -T % http://10.8.1.1:5000/andrew/Documents/$directory/%
+ 	find ~/$doc_dir$directory/ -type f -printf "%P\n" | xargs -I % curl -v -u $username:$password -T % http://10.8.1.1:5000/andrew/Documents/$directory/%
 	cd $start_dir
 }
